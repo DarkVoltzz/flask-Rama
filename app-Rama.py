708630425c10d9a1cff2f6634c049ff8e15f03ca
Adrian Rama <adrianrama06@icloud.com>
2025-05-18 14:57:39 +0200

    jenkens

--- app-Rama.py
+++ app-Rama.py
@@ -3,7 +3,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "devops de merde"
+    return "jenkins est vraiment chiant parfois"
+
+print("ald")
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
